5145c23ac57b174da2aa4f74cf28e72ee178c9f8
Jonathan Magnan <info@zzzprojects.com>
2020-10-08 10:26:17 -0400

    Added StrongName assembly

---
+++ Dapper.Transaction.StrongName.Net461/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Dapper.Transaction")]
+[assembly: AssemblyDescription("IDbTransaction extension methods for Dapper: A high performance Micro-ORM supporting SQL Server, MySQL, Sqlite, SqlCE, Firebird etc..")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Dapper.Transaction")]
+[assembly: AssemblyCopyright("ZZZ Projects")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("b95951e5-7167-4b34-9cf0-d33ac6178333")]
+
+[assembly: AssemblyVersion("2.0.35.0")]
+[assembly: AssemblyFileVersion("2.0.35.0")]
