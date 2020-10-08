49b496b3c32aac5226e82d3d379c814c5a0fbbf2
Jonathan Magnan <info@zzzprojects.com>
2020-10-08 11:39:14 -0400

    Update Lab & Add StrongName lab

---
+++ Lab.StrongName/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Dapper.Transaction;
+
+namespace Lab.StrongName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var connection = new SqlConnection("Server=localhost;Initial Catalog=Z.Dapper.Plus.Lab;Integrated Security=True;"))
+            {
+                var x1 = connection.ExecuteScalar("SELECT 1");
+            }
+
+            using (var connection = new SqlConnection("Server=localhost;Initial Catalog=Z.Dapper.Plus.Lab;Integrated Security=True;"))
+            {
+                connection.Open();
+                var trans = connection.BeginTransaction();
+                var x1 = trans.ExecuteScalar("SELECT 1");
+            }
+        }
+    }
+}
